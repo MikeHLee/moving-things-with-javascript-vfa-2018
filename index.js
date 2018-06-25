4800a0afcfa4ef1294a7d6233c4e1ba1976c2005
Michael Lee <leemichael289@gmail.com>
2018-06-25 22:43:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,22 @@ function moveDodgerLeft() { //define function to move dodger left
 }
 
 document.addEventListener('keydown', function(e) { //add this functionality to the browser in form of EventListener
-  if (e.which === 37) { //use the event listener number for left
+  if (e.which === 37) { //If the key listener returns 37,
     moveDodgerLeft() //call function
+  }
+})
+
+function moveDodgerRight() { //define function to move dodger right
+  var leftNumbers = dodger.style.left.replace('px', '') //define variable leftnumbers without "px" in string
+  var left = parseInt(leftNumbers, 10) //convert string to integer
+
+  if (left < 360) { //IF the value of left is less than 360
+    dodger.style.left = `${left + 1}px`//add 1 to its value, reconvert to original string form
+  } //otherwise do nothing
+}
+
+document.addEventListener('keydown', function(e) { //add this functionality to the browser in form of EventListener
+  if (e.which === 39) { //if the key listener returns 39
+    moveDodgerRight() //call function
   }
 })
